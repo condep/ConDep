b954ad8e95ed9debde8a74d4d4f74dc093735323
Jon Arild T�rresdal <jon@torresdal.net>
2012-01-08 00:08:41 +0100

    First atempt at using Selenium to support smoke testing in the DSL

--- ConDep.Dsl/Operations/SmokeTest/SmokeTestExtension.cs
+++ ConDep.Dsl/Operations/SmokeTest/SmokeTestExtension.cs
@@ -1,50 +1,13 @@
-using System;
 using ConDep.Dsl.Builders;
-using ConDep.Dsl.Operations.WebDeploy.Model;
-using FluentAutomation.API;
-using FluentAutomation.API.Providers;
 
 namespace ConDep.Dsl
 {
     public static class SmokeTestExtension
     {
-        public static void SmokeTest(this SetupOptions setupOptions, Action<SmokeTestOptions> action)
+        public static void SmokeTest(this SetupOptions setupOptions, string url)
         {
-            var smokeTestOperation = new SmokeTestOperation();
+            var smokeTestOperation = new SmokeTestOperation(url);
             setupOptions.AddOperation(smokeTestOperation);
-
-            action(new SmokeTestOptions());
-        }
-    }
-
-    public class SmokeTestOptions : FluentTest
-    {
-        public SmokeTestOptions()
-        {
-        }
-
-        public void Url(string url)
-        {
-            I.Open(url);
-        }
-
-        public override CommandManager I
-        {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
-        }
-    }
-
-    public class SmokeTestOperation : IOperateConDep
-    {
-        public bool IsValid(Notification notification)
-        {
-            throw new NotImplementedException();
-        }
-
-        public WebDeploymentStatus Execute(EventHandler<WebDeployMessageEventArgs> output, EventHandler<WebDeployMessageEventArgs> outputError, WebDeploymentStatus webDeploymentStatus)
-        {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ ConDep.Dsl/Operations/SmokeTest/SmokeTestOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using ConDep.Dsl.Builders;
+using ConDep.Dsl.Operations.WebDeploy.Model;
+using FluentAutomation.API.Enumerations;
+using FluentAutomation.SeleniumWebDriver;
+
+namespace ConDep.Dsl
+{
+    public class SmokeTestOperation : FluentTest, IOperateConDep
+    {
+        private readonly string _url;
+        
+        public SmokeTestOperation(string url)
+        {
+            _url = url;
+        }
+
+        public bool IsValid(Notification notification)
+        {
+            return true;
+        }
+
+        public WebDeploymentStatus Execute(EventHandler<WebDeployMessageEventArgs> output, EventHandler<WebDeployMessageEventArgs> outputError, WebDeploymentStatus webDeploymentStatus)
+        {
+            try
+            {
+                I.Use(BrowserType.Chrome);
+                I.Open(_url);
+                I.Finish();
+            }
+            catch(Exception ex)
+            {
+                var args = new WebDeployMessageEventArgs {Level = TraceLevel.Error, Message = ex.Message};
+                outputError(this, args);
+                I.Finish();
+            }
+            return webDeploymentStatus;
+        }
+    }
+}
