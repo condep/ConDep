838e9d0ce8eca8593de77e6a70985734c7843e19
Jon Arild T�rresdal <jon@torresdal.net>
2012-07-23 00:16:53 +0200

    Updated copyright year

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConDep")]
-[assembly: AssemblyCopyright("Copyright © Jon Arild Tørresdal 2011")]
+[assembly: AssemblyCopyright("Copyright © Jon Arild Tørresdal 2012")]
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
