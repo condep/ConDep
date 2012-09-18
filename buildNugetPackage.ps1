f1a62c91ee51b33989d047503b8ab7e0ca034fd7
Jon Arild T�rresdal <jon@torresdal.net>
2012-09-18 18:03:00 +0200

    fixed another path

--- buildNugetPackage.ps1
+++ buildNugetPackage.ps1
@@ -1,17 +1,14 @@
 properties {
-	$pwd = Split-Path $psake.build_script_file	
-	$build_directory  = "$pwd\Build"
-	$nuget_directory  = "$$build_directory\.nuget"
-	$nuget  = "$nuget_directory\nuget.exe"
+	$nuget  = ".nuget\nuget.exe"
 }
 
 task default -depends BuildNugetPackage
 
 task BuildNugetPackage { 
-	$nuspecFiles = get-childitem $build_directory -name -include *.nuspec
+	$nuspecFiles = get-childitem -name -include *.nuspec
 	
 	$nuspecFiles | foreach {
 		Write-Host "Creating nuget package with $_"
-		Exec { & "$nuget" pack $build_directory\$_ -verbose }
+		Exec { & "$nuget" pack $_ -verbose }
 	}
 }
