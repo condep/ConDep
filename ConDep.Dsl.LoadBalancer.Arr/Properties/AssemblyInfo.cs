852ebca192c8887c13bd22097b3e068949a139c2
Jon Arild T�rresdal <jon@torresdal.net>
2012-09-20 19:01:59 +0200

    Added assembly versioning to ConDep.Dsl.LoadBalancer.Arr

--- ConDep.Dsl.LoadBalancer.Arr/Properties/AssemblyInfo.cs
+++ ConDep.Dsl.LoadBalancer.Arr/Properties/AssemblyInfo.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConDep.Dsl.Infrastructure.Providers.ApplicationRequestRouting")]
+[assembly: AssemblyTitle("ConDep.Dsl.LoadBalancer.Arr")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConDep.Dsl.Infrastructure.Providers.ApplicationRequestRouting")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("15db55f7-b602-4a83-8e10-0520ae4fffca")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
