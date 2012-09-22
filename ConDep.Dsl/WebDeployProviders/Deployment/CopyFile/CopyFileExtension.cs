dece9f39a1064482f4ce36e9d75bd98e1ad787b3
Jon Arild T�rresdal <jon@torresdal.net>
2012-09-22 18:13:15 +0200

    Removed old code

--- ConDep.Dsl/WebDeployProviders/Deployment/CopyFile/CopyFileExtension.cs
+++ ConDep.Dsl/WebDeployProviders/Deployment/CopyFile/CopyFileExtension.cs
@@ -9,19 +9,15 @@ namespace ConDep.Dsl
 
         public static void CopyFile(this ProvideForDeployment providerOptions, string path)
         {
-            //var options = (DeploymentProviderOptions) providerOptions;
             var copyFileProvider = new CopyFileProvider(path);
             ((IProvideOptions)providerOptions).AddProviderAction(copyFileProvider);
-            //options.WebDeploySetup.ConfigureProvider(copyFileProvider);
         }
         
         public static void CopyFile(this ProvideForDeployment providerOptions, string path, Action<CopyFileOptions> copyFileOptions)
 		{
-            //var options = (DeploymentProviderOptions)providerOptions;
             var copyFileProvider = new CopyFileProvider(path);
 			copyFileOptions(new CopyFileOptions(copyFileProvider));
             ((IProvideOptions)providerOptions).AddProviderAction(copyFileProvider);
-            //options.WebDeploySetup.ConfigureProvider(copyFileProvider);
         }
     }
 }
