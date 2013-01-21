fa32435ba580c227d98b5e4ec39f35d362278eb5
Jon Arild T�rresdal <jon@torresdal.net>
2013-01-21 12:16:21 +0100

    Test

--- ConDep.Dsl.Tests/AceTests.cs
+++ ConDep.Dsl.Tests/AceTests.cs
@@ -12,7 +12,7 @@ namespace ConDep.Dsl.Tests
         [Ignore]
         public void Test()
         {
-            var loadBalancer = new AceLoadBalancer_Anm_v41("104170-ace", "noldus,2000");
+            var loadBalancer = new AceLoadBalancer_Anm_v41("104170-ace", "*******");
             var status = new StatusReporter();
             loadBalancer.BringOffline("z63os2swb01-t", "test_env_FARM", LoadBalancerSuspendMethod.Suspend, status);
         }
