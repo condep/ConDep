cdea20bbb993a6f752afde126138b46ac41a1a90
Jon Arild T�rresdal <jon@torresdal.net>
2013-01-25 14:06:04 +0100

    Another try at nservicebus issue

--- ConDep.Dsl/Operations/Application/Deployment/NServiceBus/NServiceBus.ps1
+++ ConDep.Dsl/Operations/Application/Deployment/NServiceBus/NServiceBus.ps1
@@ -25,14 +25,14 @@
 
 function Remove-NSBService($serviceName) {
 	$result = 0
-	$service = Get-Service $serviceName -ErrorAction Stop
+	$managedService = Get-Service $serviceName -ErrorAction Stop
 	
-	if ($service.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) 
+	if ($managedService.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) 
 	{ 
-		Write-Host "Stopping: $($service.DisplayName)"
-		$service.Stop()
-		$service.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped)
-		Write-Host "Stopped: $($service.DisplayName)"
+		Write-Host "Stopping: $($managedService.DisplayName)"
+		$managedService.Stop()
+		$managedService.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped)
+		Write-Host "Stopped: $($managedService.DisplayName)"
 	} 
 
 	#$service = Get-Service $serviceName -ErrorAction Stop
@@ -44,6 +44,17 @@ function Remove-NSBService($serviceName) {
 	if($result -ne 0) {
 		throw 'Unable to delete service [$serviceName]. Return code [$result].'
 	}
+
+	$managedService.Refresh()
+	
+	if($managedService.Status) {
+		while ($managedService.Status) {
+			Write-Host "Waiting for service to be deleted..."
+			Start-Sleep -s 2
+			$managedService.Refresh()
+		}
+	}
+
 	Write-Host "Removed: $($service.DisplayName)"
 	
     #C:\\WINDOWS\\system32\\sc.exe delete $serviceName
