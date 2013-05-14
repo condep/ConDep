938dcf8ef8374917d89463599e52d84015807567
Jon Arild T�rresdal <jon@torresdal.net>
2013-05-14 22:01:05 +0200

    Renamed ConDep.Client.Tests to ConDep.Node.Tests

--- ConDep.Node.Tests/Properties/AssemblyInfo.cs
+++ ConDep.Node.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConDep.Client.Test")]
+[assembly: AssemblyTitle("ConDep.Client.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ConDep.Client.Test")]
+[assembly: AssemblyProduct("ConDep.Client.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("003a3a20-cfda-46f5-acf0-b2114f120c0f")]
+[assembly: Guid("909e3fb0-33a4-4dff-9dec-4d39731a32b9")]
 
 // Version information for an assembly consists of the following four values:
 //
