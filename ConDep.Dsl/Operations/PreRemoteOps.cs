706076c540ea5399bce1a63e1b7b2f45ab406ea3
Jon Arild Torresdal <jon@torresdal.net>
2013-11-19 20:55:53 +0100

    Cleanup

--- ConDep.Dsl/Operations/PreRemoteOps.cs
+++ ConDep.Dsl/Operations/PreRemoteOps.cs
@@ -15,6 +15,7 @@ namespace ConDep.Dsl.Operations
     internal class PreRemoteOps : IOperateRemote
     {
         const string TMP_FOLDER = @"{0}\temp\ConDep\{1}";
+        const string NODE_LISTEN_URL = "http://{0}:80/ConDepNode/";
 
         public void Execute(ServerConfig server, IReportStatus status, ConDepSettings settings)
         {
@@ -26,7 +27,7 @@ namespace ConDep.Dsl.Operations
                     server.GetServerInfo().TempFolderPowerShell = string.Format(TMP_FOLDER, "$env:windir", ConDepGlobals.ExecId);
                     Logger.Info(string.Format("PowerShell temp folder is {0}", server.GetServerInfo().TempFolderDos));
 
-                    TempInstallConDepNode(status, server);
+                    TempInstallConDepNode(server);
 
                     Logger.WithLogSection("Copying PowerShell Scripts", () =>
                         {
@@ -89,22 +90,38 @@ namespace ConDep.Dsl.Operations
             filePublisher.PublishFile(srcPath, dstPath, server);
         }
 
-        private void TempInstallConDepNode(IReportStatus status, ServerConfig server)
+        private void TempInstallConDepNode(ServerConfig server)
         {
             Logger.WithLogSection("Deploying ConDep Node", () =>
                 {
-                    var listenUrl = "http://{0}:80/ConDepNode/";
-                    var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ConDepNode.exe");
+                    string path;
+
+                    var executionPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ConDepNode.exe");
+                    if (!File.Exists(executionPath))
+                    {
+                        var currentPath = Path.Combine(Directory.GetCurrentDirectory(), "ConDepNode.exe");
+                        if (!File.Exists(currentPath))
+                        {
+                            throw new FileNotFoundException("Could not find ConDepNode.exe. Paths tried: \n" +
+                                                            executionPath + "\n" + currentPath);
+                        }
+                        path = currentPath;
+                    }
+                    else
+                    {
+                        path = executionPath;
+                    }
+
                     var byteArray = File.ReadAllBytes(path);
-                    var nodePublisher = new ConDepNodePublisher(byteArray, Path.Combine(server.GetServerInfo().TempFolderPowerShell, Path.GetFileName(path)), string.Format(listenUrl, "localhost"));
+                    var nodePublisher = new ConDepNodePublisher(byteArray, Path.Combine(server.GetServerInfo().TempFolderPowerShell, Path.GetFileName(path)), string.Format(NODE_LISTEN_URL, "localhost"));
                     nodePublisher.Execute(server);
-                    if (!nodePublisher.ValidateNode(string.Format(listenUrl, server.Name), server.DeploymentUser.UserName, server.DeploymentUser.Password))
+                    if (!nodePublisher.ValidateNode(string.Format(NODE_LISTEN_URL, server.Name), server.DeploymentUser.UserName, server.DeploymentUser.Password))
                     {
                         throw new ConDepNodeValidationException("Unable to make contact witstring.Format(listenUrl, server.Name)h ConDep Node or return content from API.");
                     }
 
                     Logger.Info(string.Format("ConDep Node successfully deployed to {0}", server.Name));
-                    Logger.Info(string.Format("Node listening on {0}", string.Format(listenUrl, server.Name)));
+                    Logger.Info(string.Format("Node listening on {0}", string.Format(NODE_LISTEN_URL, server.Name)));
                 });
         }
 
