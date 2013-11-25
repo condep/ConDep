afe28ca4cee40b730f63432d6925f846510b5c5c
Jon Arild Torresdal <jon@torresdal.net>
2013-11-25 21:57:22 +0100

    Added OS Enum property

--- ConDep.Dsl/Config/OperatingSystemInfo.cs
+++ ConDep.Dsl/Config/OperatingSystemInfo.cs
@@ -11,5 +11,17 @@ namespace ConDep.Dsl.Config
         public string Name { get; set; }
         public string Version { get; set; }
         public int BuildNumber { get; set; }
+
+        public WindowsOperatingSystem OsAsEnum()
+        {
+            switch (BuildNumber)
+            {
+                case 6001: return WindowsOperatingSystem.WindowsServer2008;
+                case 7600: return WindowsOperatingSystem.WindowsServer2008R2;
+                case 7601: return WindowsOperatingSystem.WindowsServer2008R2_SP1;
+                case 9200: return WindowsOperatingSystem.WindowsServer2012;
+                default: return WindowsOperatingSystem.Unknown;
+            }
+        }
     }
 }
---
+++ ConDep.Dsl/Config/WindowsOperatingSystem.cs
@@ -0,0 +1,11 @@
+namespace ConDep.Dsl.Config
+{
+    public enum WindowsOperatingSystem
+    {
+        Unknown = 0,
+        WindowsServer2008 = 6001,
+        WindowsServer2008R2 = 7600,
+        WindowsServer2008R2_SP1 = 7601,
+        WindowsServer2012 = 9200
+    }
+}
